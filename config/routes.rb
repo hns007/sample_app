9f4a2764f4a4122513172009d9433d72a1f476ae
henaishuai <hns_work@163.com>
2014-08-30 19:38:20 +0800

    tdd develop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   get "staitic_pages/home"
   get "staitic_pages/help"
+  get "staitic_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
